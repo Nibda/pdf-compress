2ebb0633b365934200fcdc474881ac8c184547cb
Dima <dm.pavlyuk@gmail.com>
2021-03-12 18:22:20 +0200

    using argparse

--- pdf_compress.py
+++ pdf_compress.py
@@ -4,8 +4,8 @@ import time
 import PyPDF4
 import argparse
 from PIL import Image
+__author__ = 'Dmytro Pavliuk'
 
-QUALITY = 30
 CURRENT_PATH = os.path.dirname(os.path.abspath(__file__))
 DESTINATION = "OPTIMIZED" #destination directory for results
 
@@ -64,24 +64,29 @@ def get_pages(filename: str) -> list or "Error":
     return imgs
 
 
-def save_resuts(filename: str, pdf_out: object, pilobjects: list) -> str:
+def save_resuts(filename: str, pdf_out: object, pilobjects: list, quality: int) -> str:
     """Compress and save file ti destination dir"""
     fileout = filename.replace(CURRENT_PATH, CURRENT_PATH + os.sep + DESTINATION)
     fileout = os.path.normpath(fileout)
-    pdf_out.save(fileout, 'pdf', save_all=True, optimize=True, quality=QUALITY, append_images=pilobjects)
+    pdf_out.save(fileout, 'pdf', save_all=True, optimize=True, quality=quality, append_images=pilobjects)
     return fileout
 
 
 def main():
+    """Compress images in .pdf files using PIL"""
+    parser = argparse.ArgumentParser(description='Compress .pdf files')
+    parser.add_argument('--q', default=50, help='quality percent, provide integer (default: 50)')
+    args = parser.parse_args()
+    quality = int(args.q)
     count = 0
     wrongfiles = []
     start_prog = time.time()
     print("---------------------- Start --------------------- ")
-    print("Compress quality: {}%".format(QUALITY))
+    print("Compress quality: {}%".format(quality))
     mkdir_results()
     for filename in get_filenames():
         start = time.time()
-        size_in = round(os.path.getsize(filename), 2)
+        size_in = round(os.path.getsize(filename) / 1000, 2)
         imgs = get_pages(filename)
         if imgs == "Error" or not imgs:
             wrongfiles.append(filename)
@@ -91,14 +96,15 @@ def main():
         for img in imgs[1:]:
             pilobjects.append(Image.open(io.BytesIO(img)))
         try:
-            fileout = save_resuts(filename, pdf_out, pilobjects)
+            fileout = save_resuts(filename, pdf_out, pilobjects, quality)
         except Exception as err:
             print('Error: ', err)
             wrongfiles.append(filename)
+            continue
         size_out = round(os.path.getsize(fileout) / 1000, 2)
         count += 1
         print("Count: {}, \ttime: {} sec".format(count, round(time.time() - start, 2)))
-        print("Input size: {} Kb, \toutput size: {} Kb".format(size_in / 1000, size_out / 1000))
+        print("Input size: {} Kb, \toutput size: {} Kb".format(size_in, size_out))
         print('-'*50)        
     print("\nFinished {} files in {} sec".format(count, round(time.time() - start_prog, 2)))
     if wrongfiles:
@@ -108,5 +114,6 @@ def main():
 
 
 if __name__ == '__main__':
+
     main()
-    input("\n=== Presss ENTER to exit the program ===")
+    # input("\n=== Presss ENTER to exit the program ===")
